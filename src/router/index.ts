fa521e146a6d99b7abe63a5d4f778182a7137ccc
yikai <yikai@didiglobal.com>
2024-04-25 13:41:53 +0800

    自动路由unplugin-vue-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+// @ts-ignore
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
   // #/router hash模式
   // history API pushStete replaceState popstate
   // SEO -> 抓取HTML -> JS ->Vue ->SSR ->Nuxt
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
+  history: createWebHistory()
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'home',
+  //     component: HomeView
+  //   }
+  // ]
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  plugins: [VueRouter({}), vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

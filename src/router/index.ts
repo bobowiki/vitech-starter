0ad303fec117591c788b687b4e45488304603032
yikai <yikai@didiglobal.com>
2024-04-25 13:10:54 +0800

    路由相关

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
+  // #/router hash模式
+  // history API pushStete replaceState popstate
+  // SEO -> 抓取HTML -> JS ->Vue ->SSR ->Nuxt
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {

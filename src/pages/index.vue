91285b2d59b78bf06f9f470635b55dcf34307b3d
yikai <yikai@didiglobal.com>
2024-04-25 15:24:34 +0800

    结合了unocss+tailwindcss

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,7 @@ import { RouterLink } from 'vue-router/auto'
 
 <template>
   <div class="p-10">hello world</div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <!-- <div msg="hello">hello</div> -->
   <!-- <HelloWorld msg="13">11111</HelloWorld> -->
 
